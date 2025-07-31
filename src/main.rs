655428631f86efd368394d28a363343c9f777507
Bregy Malpartida <bregy@minsky.cc>
2025-07-31 18:28:31 -0500

    feat: integrate rust_decimal and enhance strategy implementation
    - Added `rust_decimal` dependency in `Cargo.toml` for precise decimal arithmetic and updated `Cargo.lock`.
    - Refactored `src/main.rs` to implement a new strategy structure for handling market data, utilizing `polars` for data manipulation.
    - Introduced `MinimalStrategy` and `StrategyState` structs to manage trading strategies and their states.
    - Streamlined the main function to process BTC data and execute strategy ticks based on Kline data.

--- src/main.rs
+++ src/main.rs
@@ -1,300 +1,154 @@
-// use apca::ApiInfo;
-// use apca::Client;
-// use apca::api::v2::order;
+use std::{any::Any, collections::HashMap};
 
-// use num_decimal::Num;
-
-use std::{env, sync::atomic::AtomicBool};
-
-use binance::{
-    account::Account,
-    api::Binance,
-    market::Market,
-    websockets::{WebSockets, WebsocketEvent},
+use binance::model::Kline;
+use greenrock::processor::load_btc_data;
+use polars::{
+    frame::DataFrame,
+    prelude::{AnyValue, DataType, IntoLazy, col},
 };
-use chrono::Utc;
+use rust_decimal::Decimal;
+// use rust_decimal::prelude::*;
 
-// #[tokio::main]
-fn main() {
-    dotenvy::dotenv().ok();
-
-    let market: Market = Binance::new(None, None);
-
-    // Order book at default depth
-    // match market.get_depth("BNBETH") {
-    //     Ok(answer) => println!("{:?}", answer),
-    //     Err(e) => println!("Error: {}", e),
-    // }
-
-    // // Order book at depth 500
-    // match market.get_custom_depth("BNBETH", 500) {
-    //     Ok(answer) => println!("{:?}", answer),
-    //     Err(e) => println!("Error: {}", e),
-    // }
-
-    // // Latest price for ALL symbols
-    // match market.get_all_prices() {
-    //     Ok(answer) => println!("{:?}", answer),
-    //     Err(e) => println!("Error: {:?}", e),
-    // }
-
-    // // Latest price for ONE symbol
-    // match market.get_price("BNBETH") {
-    //     Ok(answer) => println!("{:?}", answer),
-    //     Err(e) => println!("Error: {:?}", e),
-    // }
-
-    // // Current average price for ONE symbol
-    // match market.get_average_price("BNBETH") {
-    //     Ok(answer) => println!("{:?}", answer),
-    //     Err(e) => println!("Error: {:?}", e),
-    // }
-
-    // // Best price/qty on the order book for ALL symbols
-    // match market.get_all_book_tickers() {
-    //     Ok(answer) => println!("{:?}", answer),
-    //     Err(e) => println!("Error: {:?}", e),
-    // }
-
-    // // Best price/qty on the order book for ONE symbol
-    // match market.get_book_ticker("BNBETH") {
-    //     Ok(answer) => println!(
-    //         "Bid Price: {}, Ask Price: {}",
-    //         answer.bid_price, answer.ask_price
-    //     ),
-    //     Err(e) => println!("Error: {:?}", e),
-    // }
-
-    // // 24hr ticker price change statistics
-    match market.get_24h_price_stats("BTCUSDT") {
-        Ok(answer) => println!(
-            "Open Price: {}, Higher Price: {}, Lower Price: {:?}",
-            answer.open_price, answer.high_price, answer.low_price
-        ),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    // // last 10 5min klines (candlesticks) for a symbol:
-    // match market.get_klines("BNBETH", "1m", 10, None, None) {
-    //     Ok(klines) => {
-    //         match klines {
-    //             binance::model::KlineSummaries::AllKlineSummaries(klines) => {
-    //                 let kline: KlineSummary = klines[0].clone(); // You need to iterate over the klines
-    //                 println!(
-    //                     "Open: {}, High: {}, Low: {}",
-    //                     kline.open, kline.high, kline.low
-    //                 )
-    //             }
-    //         }
-    //     }
-    //     Err(e) => println!("Error: {}", e),
-    // }
-
-    let api_key = Some(env::var("BINANCE_API_KEY").unwrap());
-    let secret_key = Some(env::var("BINANCE_SECRET_KEY").unwrap());
-
-    let account: Account = Binance::new(api_key, secret_key);
-
-    match account.get_account() {
-        Ok(answer) => println!(
-            "{:?}",
-            answer
-                .balances
-                .iter()
-                .filter(|instrument| instrument.locked.parse::<f64>().unwrap() > 0.0)
-                .collect::<Vec<_>>()
-        ),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    let keep_running = AtomicBool::new(true); // Used to control the event loop
-    let kline = "btcusdt@kline_1s".to_string();
-
-    let mut now = Utc::now();
-
-    let mut web_socket = WebSockets::new(|event: WebsocketEvent| {
-        if let WebsocketEvent::Kline(kline_event) = event {
-            let current = Utc::now();
-            let diff = current - now;
-            now = current;
-            // println!("diff: {:?}", diff);
-            println!(
-                "Symbol: {}, high: {}, low: {}, diff (ms): {}",
-                kline_event.kline.symbol,
-                kline_event.kline.low,
-                kline_event.kline.high,
-                // kline_event.kline.open,
-                // kline_event.kline.close,
-                // kline_event.kline.open_time,
-                diff.num_milliseconds(),
-            );
-        };
-        Ok(())
-    });
-
-    web_socket.connect(&kline).unwrap(); // check error
-    if let Err(e) = web_socket.event_loop(&keep_running) {
-        println!("Error: {:?}", e);
-    }
-
-    web_socket.disconnect().unwrap();
+#[derive(Clone)]
+enum StrategyTraitKind {
+    Short,
+    Long,
 }
 
-// use async_trait::async_trait;
-// use graph_flow::{
-//     Context, ExecutionStatus, FlowRunner, GraphBuilder, GraphStorage, InMemoryGraphStorage,
-//     InMemorySessionStorage, NextAction, Session, SessionStorage, Task, TaskResult,
-// };
-// use rig::prelude::*;
-// use rig::{completion::Prompt, providers};
-// use std::env;
-// use std::sync::Arc;
+#[derive(Clone)]
+struct StrategyTrait {
+    id: String,
+    kind: StrategyTraitKind,
+    value: Decimal,
+    start_value: Decimal,
+    end_value: Option<Decimal>,
+    // start: DateTime<Utc>,
+    // end: DateTime<Utc>,
+}
 
-// // We have 2 tasks in this simple example:
-// // 1. HelloTask - greets the user by name
-// // 2. ExcitementTask - adds excitement to the greeting
-// struct HelloTask;
+#[derive(Clone)]
+struct StrategyState {
+    data_scope: DataFrame,
+    traits: HashMap<String, StrategyTrait>,
+}
 
-// #[async_trait]
-// impl Task for HelloTask {
-//     async fn run(&self, context: Context) -> graph_flow::Result<TaskResult> {
-//         let name: String = context.get_sync("name").unwrap();
-//         let greeting = format!("Hello, {}", name);
-//         // Store result for next task
-//         context.set("greeting", greeting.clone()).await;
+impl Default for StrategyState {
+    fn default() -> Self {
+        Self {
+            data_scope: DataFrame::new(vec![]).unwrap(),
+            traits: HashMap::new(),
+        }
+    }
+}
 
-//         let client = providers::openrouter::Client::new(
-//             &env::var("OPENROUTER_API_KEY").expect("OPENROUTER_API_KEY not set"),
-//         );
+trait Starting {
+    // fn start(&self, state: &StrategyState) -> StrategyState;
+    fn tick(&self, state: &StrategyState, tick: Option<Kline>) -> StrategyState;
+}
 
-//         // Create agent with a single context prompt
-//         let comedian_agent = client
-//             .agent("deepseek/deepseek-chat-v3-0324:free")
-//             .preamble("You are a comedian here to entertain the user using humour and jokes.")
-//             .build();
+struct MinimalStrategy {
+    state: StrategyState,
+}
 
-//         // Prompt the agent and print the response
-//         let response = comedian_agent.prompt("Entertain me!").await.unwrap();
+impl MinimalStrategy {
+    fn new(data_scope: DataFrame) -> Self {
+        Self {
+            state: StrategyState {
+                data_scope,
+                traits: HashMap::new(),
+            },
+        }
+    }
+}
 
-//         println!("{response}");
+impl Starting for MinimalStrategy {
+    fn tick(&self, state: &StrategyState, tick: Option<Kline>) -> StrategyState {
+        if let Some(tick) = tick {
+            let close = tick.close.parse::<f64>().unwrap();
 
-//         // using NextAction::Continue to indicate we want to proceed to the next task,
-//         // but we want to advance just one step and give control back to the workflow manager
-//         // We can also use NextAction::ContinueAndExecute if we want to execute the next task immediately
-//         Ok(TaskResult::new(Some(greeting), NextAction::Continue))
-//     }
-// }
+            println!("{}", close);
+        }
 
-// // Define a task that adds excitement
-// struct ExcitementTask;
+        (*state).clone()
+    }
+}
 
-// #[async_trait]
-// impl Task for ExcitementTask {
-//     async fn run(&self, context: Context) -> graph_flow::Result<TaskResult> {
-//         let greeting: String = context.get_sync("greeting").unwrap();
-//         let excited = format!("{} !!!", greeting);
+fn main() {
+    let df = load_btc_data("processed_btc_data/2021-01.parquet");
 
-//         Ok(TaskResult::new(Some(excited), NextAction::End))
-//     }
-// }
+    let mut strategy = MinimalStrategy::new(df.clone());
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     dotenvy::dotenv().ok();
+    let df = df
+        .clone()
+        .lazy()
+        .select([
+            col("timestamp"),
+            col("open"),
+            col("high"),
+            col("low"),
+            col("close"),
+            col("volume"),
+        ])
+        .collect()
+        .unwrap();
 
-//     // Create storage instances
-//     let session_storage = Arc::new(InMemorySessionStorage::new());
-//     let graph_storage = Arc::new(InMemoryGraphStorage::new());
+    for i in 0..df.shape().0 {
+        let row = df.get_row(i).unwrap();
 
-//     // Build a simple workflow
-//     let hello_task = Arc::new(HelloTask);
-//     let hello_task_id = hello_task.id().to_string();
-//     let excitement_task = Arc::new(ExcitementTask);
-//     let excitement_task_id = excitement_task.id().to_string();
+        let timestamp = match row.0[0] {
+            AnyValue::Datetime(value, _, _) => value,
+            _ => panic!("Invalid timestamp"),
+        };
 
-//     let graph = Arc::new(
-//         GraphBuilder::new("greeting_workflow")
-//             .add_task(hello_task)
-//             .add_task(excitement_task)
-//             .add_edge(&hello_task_id, &excitement_task_id) // Connect the tasks
-//             .build(),
-//     );
+        let open = match row.0[1] {
+            AnyValue::String(value) => value.parse::<f64>().unwrap(),
+            _ => panic!("Invalid open"),
+        };
 
-//     // Store the graph in graph storage
-//     graph_storage
-//         .save("greeting_workflow".to_string(), graph.clone())
-//         .await?;
+        let high = match row.0[2] {
+            AnyValue::Float64(value) => value,
+            _ => panic!("Invalid high"),
+        };
 
-//     // Create a session with initial context
-//     let session_id = "session_001".to_string();
-//     let session = Session::new_from_task(session_id.clone(), &hello_task_id);
+        let low = match row.0[3] {
+            AnyValue::Float64(value) => value,
+            _ => panic!("Invalid low"),
+        };
 
-//     // Set up context with input data
-//     session.context.set("name", "Batman".to_string()).await;
-//     // Save the session
-//     session_storage.save(session.clone()).await?;
+        let close = match row.0[4] {
+            AnyValue::Float64(value) => value,
+            _ => panic!("Invalid close"),
+        };
 
-//     println!("Starting simple workflow with FlowRunner\n");
-//     println!("Session ID: {}", session.id);
-//     println!("Initial task: {}\n", session.current_task_id);
+        let volume = match row.0[5] {
+            AnyValue::String(value) => value.parse::<f64>().unwrap(),
+            _ => panic!("Invalid volume"),
+        };
 
-//     // Create a FlowRunner that hides the load / execute / save boilerplate
-//     let runner = FlowRunner::new(graph.clone(), session_storage.clone());
+        let tick = Kline {
+            symbol: "BTCUSDT".to_string(),
+            interval: "1m".to_string(),
+            ignore_me: "".to_string(),
+            open_time: timestamp,
+            open: open.to_string(),
+            high: high.to_string(),
+            low: low.to_string(),
+            close: close.to_string(),
+            volume: volume.to_string(),
+            close_time: timestamp,
+            first_trade_id: 0,
+            last_trade_id: 0,
+            number_of_trades: 0,
+            is_final_bar: false,
+            quote_asset_volume: "".to_string(),
+            taker_buy_base_asset_volume: "".to_string(),
+            taker_buy_quote_asset_volume: "".to_string(),
+        };
 
-//     // Execute until completion
-//     loop {
-//         let execution_result = runner.run(&session_id).await?;
+        strategy.state = strategy.tick(&strategy.state, Some(tick));
+    }
 
-//         if let Some(response) = &execution_result.response {
-//             println!("Task response: {}", response);
-//         }
+    // println!("{}", df.shape().0);
 
-//         println!("Execution status: {:?}", execution_result.status);
-
-//         match execution_result.status {
-//             ExecutionStatus::Completed => {
-//                 println!("Workflow completed successfully!");
-//                 break;
-//             }
-//             ExecutionStatus::Paused {
-//                 next_task_id,
-//                 reason,
-//             } => {
-//                 println!(
-//                     "Workflow paused, will continue to task: {} (reason: {}) – continuing...\n",
-//                     next_task_id, reason
-//                 );
-//                 continue;
-//             }
-//             ExecutionStatus::WaitingForInput => {
-//                 println!("Workflow waiting for user input – continuing...\n");
-//                 continue;
-//             }
-//             ExecutionStatus::Error(err) => {
-//                 println!("Error occurred: {}", err);
-//                 break;
-//             }
-//         }
-//     }
-
-//     // Demonstrate session persistence by retrieving final session
-//     let final_session = session_storage
-//         .get(&session_id)
-//         .await?
-//         .ok_or("Session not found")?;
-
-//     println!("\nFinal session state:");
-//     println!("Session ID: {}", final_session.id);
-//     println!("Current task: {}", final_session.current_task_id);
-//     if let Some(status) = &final_session.status_message {
-//         println!("Final status: {}", status);
-//     }
-
-//     // Demonstrate retrieving stored values from context
-//     if let Some(greeting) = final_session.context.get::<String>("greeting").await {
-//         println!("Stored greeting: {}", greeting);
-//     }
-
-//     println!("\nWorkflow execution finished");
-//     Ok(())
-// }
+    println!("{}", strategy.state.data_scope);
+}
