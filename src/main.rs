c7a212fb2527be20b2318223ede5028288c31e1d
Bregy Malpartida <bregy@minsky.cc>
2025-08-01 18:38:49 -0500

    refactor: clean up Jupyter notebook and remove commented-out code
    - Removed unnecessary output and commented-out code from `strategy_exploration_0.ipynb` to streamline the notebook.
    - Updated execution counts to `null` for cells that are not intended to be executed, improving clarity and usability.

--- src/main.rs
+++ src/main.rs
@@ -48,14 +48,6 @@ async fn main() {
 
     let bot = Bot::from_env();
 
-    // teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-    //     println!("Received message: {:?}", msg);
-    //     bot.send_message(msg.chat.id, "Hello, world!").await?;
-    //     // bot.send_dice(msg.chat.id).await?;
-    //     Ok(())
-    // })
-    // .await;
-
     Command::repl(bot, answer).await;
 }
 
